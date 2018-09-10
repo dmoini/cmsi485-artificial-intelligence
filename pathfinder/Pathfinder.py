7d2d7dbe2a2cfeed15fe8b5c471f3f2b77930623
Donovan Moini <donovanmoini@gmail.com>
2018-09-10 09:36:52 -0700

    Finished solve(). Will cleanup later

--- pathfinder/Pathfinder.py
+++ pathfinder/Pathfinder.py
@@ -19,11 +19,52 @@ class Pathfinder:
     # example returned list might look like:
     # ["U", "R", "R", "U"]
     def solve(problem):
-        # TODO: Implement breadth first tree search!
-        frontier = []
-        graveyard = set
-        path = {}
+        queue = [SearchTreeNode(problem.initial, None, None)]
+        graveyard = set()  # Contains states, not actual nodes
+        while len(queue) != 0:
+            current_node = queue.pop(0)
+            # print(f'Current node: str(){current_node.state})')
+            # front = path[-1]
+            if problem.goalTest(current_node.state):
+                print(str(graveyard))
+                return Pathfinder.create_path(current_node)  # TODO: fix
+            elif current_node.state not in graveyard:
+                # parent = current_node
+                for available_moves in problem.transitions(current_node.state):
+                    # print(f'Available node: {available_moves[1]})')
+                    # TODO: shorten code below into single line
+                    next_state = available_moves[1]
+                    action = available_moves[0]
+                    parent = current_node
+                    available_node = SearchTreeNode(next_state, action, parent)
+                    if available_node.state not in graveyard:
+                        # print("QUEUEEEED")
+                        queue.append(available_node)
+                    # new_path = [path]
+                    # new_path.append(SearchTreeNode(available_move[1], available_move[0], parent))
+                    # queue.append(new_path)
+                graveyard.add(current_node.state)
         return []
+
+    # STN is a SearchTreeNode
+    # create_path returns a list of "DIRECTION" where
+    # STN is the SearchTreeNode given
+    # and path is the path from the goal state to the initial state
+    def create_path(STN):
+        path = []
+        current_node = STN
+        while current_node.parent is not None:
+            path.insert(0, current_node.action)
+            current_node = current_node.parent
+        print(path)
+        return path
+
+    # def solve(start, end):
+    #     queue = [SearchTreeNode(start, None, None)]
+    #     graveyard = ()
+    #     while len(queue) != 0:
+    #         parent = queue.pop(0)
+    #         for available_moves in transitions(parent):
 
 
 class PathfinderTests(unittest.TestCase):
