d7dfe5443064fac58219af034bc65188c19bbbc2
Donovan Moini <donovanmoini@gmail.com>
2018-11-16 19:10:38 -0800

    this is stupid

--- homework-3/ad_engine.py
+++ homework-3/ad_engine.py
@@ -14,32 +14,39 @@ from pomegranate import *
 
 class AdEngine:
     
+    """
+    Responsible for initializing the Decision Network of the
+    AdEngine from the structure discovered by Tetrad
+    
+    :param string data_file: path to csv file containing data on which
+    the network's parameters are to be learned
+    :param tuple structure: tuple of tuples specifying parental
+    relationships between variables in the network; see Pomegranate docs
+    for the expected format. Example:
+        ((), (0), (1)) represents nodes: [0] -> [1] -> [2]
+    :param list dec_vars: list of string names of variables to be
+    considered decision points for the agent. Example:
+        ["Ad1", "Ad2"]
+    :param dict util_map: discrete, tabular, utility map whose keys
+    are variables in network that are parents of a utility node, and
+    values are dictionaries mapping that variable's values to a utility
+    score, e.g.
+        {
+        "X": {0: 20, 1: -10}
+        }
+    represents a utility node with single parent X whose value of 0
+    has a utility score of 20, and value 1 has a utility score of -10
+    """
+
     def __init__(self, data_file, structure, dec_vars, util_map):
-        """
-        Responsible for initializing the Decision Network of the
-        AdEngine from the structure discovered by Tetrad
-        
-        :param string data_file: path to csv file containing data on which
-        the network's parameters are to be learned
-        :param tuple structure: tuple of tuples specifying parental
-        relationships between variables in the network; see Pomegranate docs
-        for the expected format. Example:
-          ((), (0), (1)) represents nodes: [0] -> [1] -> [2]
-        :param list dec_vars: list of string names of variables to be
-        considered decision points for the agent. Example:
-          ["Ad1", "Ad2"]
-        :param dict util_map: discrete, tabular, utility map whose keys
-        are variables in network that are parents of a utility node, and
-        values are dictionaries mapping that variable's values to a utility
-        score, e.g.
-          {
-            "X": {0: 20, 1: -10}
-          }
-        represents a utility node with single parent X whose value of 0
-        has a utility score of 20, and value 1 has a utility score of -10
-        """
-        # TODO! You decide the attributes and initialization of the BN!
-        return
+        self.data_file = data_file
+        data = np.genfromtxt(data_file, names=True, delimiter=',')
+        self.names = data.dtype.names
+        self.network = BayesianNetwork.from_structure(data.view((int, len(self.names))), structure, state_names=self.names)
+        self.dec_vars = dec_vars
+        # dictionary with all decisions and tuple of possibilities to find all possibilities
+        self.structure = structure #TUPLE of TUPLES
+        self.util_map = util_map
         
     def decide(self, evidence):
         """
@@ -54,14 +61,33 @@ class AdEngine:
         """
         best_combo, best_util = None, -math.inf
         # TODO: Rest of the implementation goes here!
+        # print(evidence)
+        cpts = self.network.predict_proba(evidence)
+        print(len(cpts))
+        print(cpts)
         return best_combo
 
 
 class AdEngineTests(unittest.TestCase):
-    def test_defendotron_ad_engine(self):
+    def test_defendotron_ad_engine_t1(self):
         engine = AdEngine(
             data_file = 'hw3_data.csv',
             dec_vars = ["Ad1", "Ad2"],
+            #            P   A   G        I     T        F        H   S        Ad2 Ad1
+            structure = ((), (), (0, 9,), (6,), (0, 1,), (1, 8,), (), (2, 5,), (), ()),
+            util_map = {'S': {0: 0, 1: 5000, 2: 17760}}
+        )
+        self.assertEqual(engine.decide({"G": 0}), {"Ad1": 0, "Ad2": 1})
+        self.assertEqual(engine.decide({"F": 1}), {"Ad1": 1, "Ad2": 0})
+        self.assertEqual(engine.decide({"G": 1, "T": 0}), {"Ad1": 1, "Ad2": 1})
+        
+    def test_defendotron_ad_engine_t2(self):
+        engine = AdEngine(
+            data_file = 'hw3_data.csv',
+            # [!] Note: in this example, say we are only deciding upon the ad
+            # video (Ad1); our engine's results should adapt accordingly (see
+            # tests below)
+            dec_vars = ["Ad1"],
             # TODO: Current structure is blank; you need to fill this in using
             # the results from the Tetrad analysis!
             structure = (),
@@ -69,9 +95,9 @@ class AdEngineTests(unittest.TestCase):
             # example; see format of util_map in spec and above!
             util_map = {}
         )
-        self.assertEqual(engine.decide({"G": 0}), {"Ad1": 0, "Ad2": 1})
-        self.assertEqual(engine.decide({"F": 1}), {"Ad1": 1, "Ad2": 0})
-        self.assertEqual(engine.decide({"G": 1, "T": 0}), {"Ad1": 1, "Ad2": 1})
+        self.assertEqual(engine.decide({"A": 1}), {"Ad1": 0})
+        self.assertEqual(engine.decide({"P": 1, "A": 0}), {"Ad1": 1})
+        self.assertEqual(engine.decide({"A": 1, "G": 1, "T": 1}), {"Ad1": 0})
 
 if __name__ == "__main__":
     unittest.main()
