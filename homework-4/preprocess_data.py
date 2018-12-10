c28f8a844d36626e60a2bad2dff52202f374dc9b
Donovan Moini <donovanmoini@gmail.com>
2018-12-10 14:30:55 -0800

    It is working now

--- homework-4/preprocess_data.py
+++ homework-4/preprocess_data.py
@@ -3,34 +3,29 @@ from sklearn.impute import SimpleImputer, MissingIndicator
 import numpy as np
 import pandas as pd
 
-def process():
-    data = np.genfromtxt('income-training.csv', dtype=str, delimiter=',')
+def preprocess(data_file):
+    data = np.genfromtxt(data_file, dtype=str, delimiter=',')
+    data = [[feature.strip(' ') for feature  in line] for line in data]
     # print(data)
-    # 
     df = pd.DataFrame(data, dtype=str)
-    imp = SimpleImputer(missing_values='?', strategy='constant', fill_value=-1)    
+    imp = SimpleImputer(missing_values='?', strategy='most_frequent')    
     impdf = imp.fit_transform(df)
-    print(impdf)
 
     # indicator = MissingIndicator(missing_values='?', features='all')
     # mask_all = indicator.fit_transform(df)
     # print(mask_all)
-
-
-
-
     # print(imp)
     # imp.fit(data)
     # print(imp.fit(data))
     # imp = SimpleImputer(missing_values='?', strategy="most_frequent")
     # print(imp)
     
-    
     # NOTE: SAFE BELOW
-    # enc = preprocessing.OrdinalEncoder()
-    # enc.fit(data)
-    # data = enc.transform(data)
-    # est = preprocessing.KBinsDiscretizer(n_bins=[3, 8, 16, 3, 6, 14, 6, 5, 2, 2, 2, 3, 195, 2], encode='ordinal').fit(data)
-    # print(est.transform(data))
+    enc = preprocessing.OrdinalEncoder()
+    enc.fit(data)
+    data = enc.transform(data)
+    est = preprocessing.KBinsDiscretizer(n_bins=[3, 8, 16, 3, 6, 14, 6, 5, 2, 2, 2, 3, 195, 2], encode='ordinal').fit(data)
+    print(est.transform(data))
+    return est.transform(data)
 
-process()
+preprocess('income-training.csv')
