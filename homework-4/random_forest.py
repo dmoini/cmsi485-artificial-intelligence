ce6595922ffa40c00e646dfa73e95dcca525fddc
Donovan Moini <donovanmoini@gmail.com>
2018-12-13 19:02:15 -0800

    Completed random_forest.py

--- homework-4/random_forest.py
+++ homework-4/random_forest.py
@@ -1,1 +1,9 @@
 import preprocess_data
+from sklearn.ensemble import RandomForestClassifier
+
+def train_random_forest():
+    data = preprocess_data.preprocess('income-training.csv', 'income-test.csv')
+    training_X, training_Y = data[0][:,0:13], data[0][:,13]
+    test_X, test_Y = data[1][:,0:13], data[1][:,13]
+    clf = RandomForestClassifier().fit(training_X, training_Y)
+    return clf.score(test_X, test_Y)
