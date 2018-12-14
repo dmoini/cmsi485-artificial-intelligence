5496c9caf07d34be568975f453c24971b4e23b9a
Donovan Moini <donovanmoini@gmail.com>
2018-12-13 18:00:09 -0800

    Fixed horrible spacing by @ianlizzo

--- homework-4/preprocess_data.py
+++ homework-4/preprocess_data.py
@@ -9,32 +9,25 @@ from sklearn.preprocessing import OrdinalEncoder
 
 
 def preprocess(training, test):
-    training_data = stripQuotes(np.genfromtxt(
-        training, dtype=str, delimiter=','))
+    training_data = stripQuotes(np.genfromtxt(training, dtype=str, delimiter=','))
     test_data = stripQuotes(np.genfromtxt(test, dtype=str, delimiter=','))
     training_df = pd.DataFrame(training_data, dtype=str)
     test_df = pd.DataFrame(test_data, dtype=str)
-    imputer = SimpleImputer(
-        missing_values='?', strategy='most_frequent').fit(training_df)
+    imputer = SimpleImputer(missing_values='?', strategy='most_frequent').fit(training_df)
     imp_training = imputer.transform(training_df)
     imp_test = imputer.transform(test_df)
     ct = ColumnTransformer(
         [("age", KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='kmeans'), [0]),
          ("work/edu", OrdinalEncoder(), [1, 2]),
-         ("edu-num", KBinsDiscretizer(n_bins=5,
-                                      encode='ordinal', strategy='kmeans'), [3]),
+         ("edu-num", KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='kmeans'), [3]),
          ("mar/occ/rel/race/sex", OrdinalEncoder(), [4, 5, 6, 7, 8]),
-         ("cg", KBinsDiscretizer(n_bins=3,
-                                 encode='ordinal', strategy='kmeans'), [9]),
-         ("cl", KBinsDiscretizer(n_bins=3,
-                                 encode='ordinal', strategy='kmeans'), [10]),
-         ("hrs", KBinsDiscretizer(n_bins=3,
-                                  encode='ordinal', strategy='kmeans'), [11]),
+         ("cg", KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='kmeans'), [9]),
+         ("cl", KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='kmeans'), [10]),
+         ("hrs", KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='kmeans'), [11]),
          ("nat", OrdinalEncoder(), [12])], remainder='passthrough').fit(imp_training)
     ct_training = ct.transform(imp_training)
     ct_test = ct.transform(imp_test)
     return (ct_training, ct_test)
-
 
 def stripQuotes(data):
     return [[feature.strip(' ') for feature in line] for line in data]
