ecae5412c093c4951cad29e590215a03ff5ea692
Donovan Moini <donovanmoini@gmail.com>
2018-12-13 18:53:28 -0800

    Completed naive_bayes.py

--- homework-4/naive_bayes.py
+++ homework-4/naive_bayes.py
@@ -1,27 +1,9 @@
 import preprocess_data
-from sklearn import datasets
-from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import MultinomialNB
 
 def train_NBC():
-    data = preprocess_data.preprocess('income-training.csv')
-    # print(data)
-    X = [[a for a in b[:13]] for b in data[:10]]
-    Y = [[b[13]] for b in data[:10]]
-    # print(X)
-    print(Y)
-    # test_data = preprocess_data.preprocess('income-test.csv')
-    # clf = ComplementNB()
-    # print(clf.fit(X, Y))
-
-    
-    # print(data)
-    # iris = datasets.load_iris()
-    # print(iris.data)
-
-
-    # gnb = GaussianNB()
-    # y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-    # y_pred = gnb.fit(data, test_data).predict(data)
-    # print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred).sum()))
-
-train_NBC()
+    data = preprocess_data.preprocess('income-training.csv', 'income-test.csv')
+    training_X, training_Y = data[0][:,0:13], data[0][:,13]
+    test_X, test_Y = data[1][:,0:13], data[1][:,13]
+    clf = MultinomialNB().fit(training_X, training_Y)
+    return clf.score(test_X, test_Y)
